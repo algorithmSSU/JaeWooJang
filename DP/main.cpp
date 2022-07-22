113e5325b78efb86a8164025bc053da4d1c34e14
Arkady1356 <arkady1356@naver.com>
2022-07-22 17:41:13 +0900

    8주차 BackJoon 문제풀이

---
+++ DP/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+extern int problem1463();
+extern int problem2193();
+extern int problem10844();
+extern int problem9252();
+//extern int problem1328();
+extern int problem11049();
+//extern int problem14003();
+
+int main() {
+	problem1463();
+	problem2193();
+	problem10844();
+	problem9252();
+	problem11049();
+}
