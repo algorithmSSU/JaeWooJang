9e104af48b8b0b704cf256d227529dfc54bcf7ee
Jaewoo Jang <akrady1356@naver.com>
2022-08-08 15:53:02 +0900

    10주차 Programmers Skill Check & 누락 문제 풀이

--- Greedy/1744.cpp
+++ Greedy/1744.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -9,11 +10,42 @@ int problem1744() {
 	int n;
 	cin >> n;
 
-	priority_queue<int> arr;
+	vector<int> positive_arr;
+	vector<int> negative_arr;
+
 	for (int i = 0; i < n; ++i) {
 		cin >> input;
-		arr.push(input);
+		if (input > 0)
+			positive_arr.emplace_back(input);
+		else
+			negative_arr.emplace_back(input);
 	}
+
+	long long sum = 0;
+	sort(positive_arr.begin(), positive_arr.end());
+	sort(negative_arr.begin(), negative_arr.end(), std::greater<>());
+
+	int i, j;
+	for (i = positive_arr.size() - 1; i >= 1; i -= 2) {
+		int tmp1 = positive_arr[i] * positive_arr[i - 1];
+		int tmp2 = positive_arr[i] + positive_arr[i - 1];
+
+		sum += (tmp1 > tmp2) ? tmp1 : tmp2;
+	}
+
+	for (j = negative_arr.size() - 1; j >= 1; j -= 2) {
+		int tmp1 = negative_arr[j] * negative_arr[j - 1];
+		int tmp2 = negative_arr[j] + negative_arr[j - 1];
+
+		sum += (tmp1 > tmp2) ? tmp1 : tmp2;
+	}
+
+	if (i >= 0)
+		sum += positive_arr[i];
+	if (j >= 0)
+		sum += negative_arr[j];
+
+	cout << sum;
 
 	return 0;
 }
--- Greedy/main.cpp
+++ Greedy/main.cpp
@@ -6,12 +6,14 @@ extern int problem11047();
 extern int problem1715();
 extern int problem1931();
 //extern int problem1541();
+extern int problem1744();
 
 int main() {
 	//problem11047();
 	//problem1715();
-	problem1931();
+	//problem1931();
 	//problem1541();
+	problem1744();
 
 	return 0;
 }
